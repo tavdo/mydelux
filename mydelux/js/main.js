e0b6899fb0698e9e30fbf084e0e75b3da96a2095
tavdo <temo.tavdgiridze@icloud.com>
2025-09-21 03:44:08 +0400

    Add files via upload

---
+++ mydelux/js/main.js
@@ -0,0 +1,5 @@
+const menuBtn = document.getElementById('menuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
+});
